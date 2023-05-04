9a35689b413e24a36cbfd63b19b75dbcc94ec7f5
luannsct <luan.nsct@gmail.com>
2023-05-03 22:14:42 -0300

    Criando Lista de Tarefas com parse-body

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const  path  = require('path');
 
 const app = express();
+
+app.use( bodyParser.json()); //Para suportar JSON ENCODED 
+app.use(bodyParser.urlencoded({ //PARA SUPORTAR URLENCODE 
+    extended:true
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine','html');
@@ -9,11 +15,30 @@ app.use('/public', express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, '/views'));
 
 
+//criando lista de tarefas
+
+var tarefas = [];
+
+app.post('/',(req,res)=>{
+    tarefas.push(req.body.tarefa);
+    res.render('index',{tarefasList:tarefas});
+})
+
+
 app.get('/',(req,res)=>{
 
-    res.render('index',{nome:'Luan'});
+    res.render('index',{tarefasList:tarefas});
 
 });
+
+app.get('/deletar/:id',(req,res)=>{
+    tarefas = tarefas.filter((val,index)=>{
+        if(index != req.params.id){
+            return val;
+        }
+    })
+    res.render('index',{tarefasList:tarefas});
+})
 
 app.listen(5000,()=>{
     console.log('Servidor Online');
